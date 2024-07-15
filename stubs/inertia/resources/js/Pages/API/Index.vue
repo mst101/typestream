8473745cf9f0b3b2e58d3b972016f18c68f21d65
Mark Thompson <mst101@users.noreply.github.com>
2024-07-15 10:50:49 +0000

    Ensure Inertia pages are children of AppLayout

--- stubs/inertia/resources/js/Pages/API/Index.vue
+++ stubs/inertia/resources/js/Pages/API/Index.vue
@@ -1,6 +1,6 @@
 <script setup>
 import ApiTokenManager from '@/Pages/API/Partials/ApiTokenManager.vue';
-import AppLayout from '@/Layouts/AppLayout.vue';
+import PageContainer from "@/Pages/PageContainer.vue";
 
 defineProps({
     tokens: Array,
@@ -10,7 +10,7 @@ defineProps({
 </script>
 
 <template>
-    <AppLayout title="API Tokens">
+    <PageContainer title="API Tokens">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
                 API Tokens
@@ -26,5 +26,5 @@ defineProps({
                 />
             </div>
         </div>
-    </AppLayout>
+    </PageContainer>
 </template>
